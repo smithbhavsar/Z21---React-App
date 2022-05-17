171f73ccca649315fda701330e625594d50b231f
Smith Bhavsar <smithbhavsar1328@gmail.com>
2022-05-17 19:24:28 +0530

    Added Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
 function Footer(){
-    const year = new Date().getFullYear;
     return(
         <footer>
-            <p>Copyright @{year} </p>
+            <p>Copyright @ Smith Bhavsar </p>
         </footer>
     );
 }
